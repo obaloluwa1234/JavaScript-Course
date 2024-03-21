cf7baa5501df89c893972af36151eccfcc9b251b
obaloluwa1234 <mrsfakowajo2016@gmail.com>
2024-03-21 04:26:57 +0100

    dom manipulations and css added

--- js/rock.js
+++ js/rock.js
@@ -1,3 +1,21 @@
+let score = JSON.parse(localStorage.getItem('score')) || {
+  wins: 0,
+  losses: 0,
+  ties: 0
+};
+
+updateScoreElement();
+
+
+ /*
+if (!score) {
+  score= {
+    wins: 0,
+    losses: 0,
+    ties: 0
+  }
+}
+*/
 function playGame(playerMove) {
   let computerMove = pickComputerMove();
 
@@ -30,8 +48,26 @@ function playGame(playerMove) {
             else if (computerMove = 'scissors') { result = 'You win'
             }
       }
-        alert(`You picked ${playerMove}. Computer picked ${computerMove}. ${result}`)
+
+      if (result === 'You win') {
+        score.wins += 1;
+      } else if (result === 'You lose') {score.losses += 1;
+      }else if (result === 'Tie') {
+        score.ties += 1;
+      }
+
+    localStorage.setItem('score', JSON.stringify(score));
+    
+    updateScoreElement();
+
+    document.querySelector('.js-result').innerHTML = result;
+
+    document.querySelector('.js-moves').innerHTML = `You ${playerMove} -${computerMove} Computer`;
    }
+
+    function updateScoreElement() {
+      document.querySelector('.js-score').innerHTML = `wins: ${score.wins}, losses: ${score.losses}, ties: ${score.ties}`;
+    }
   
 function pickComputerMove() {
   const randomNumber = Math.random(); 
